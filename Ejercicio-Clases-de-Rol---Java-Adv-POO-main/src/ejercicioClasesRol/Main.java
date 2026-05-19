4d79cc2d747f800f10095459b57e6a8d3cb829f7
Abel Gutierrez Gomez <a.gutierrez.gomez-landero@iescristobaldemonroy.es>
2026-05-19 12:11:48 +0200

    Añadir JavaDoc a los ficheros

---
+++ Ejercicio-Clases-de-Rol---Java-Adv-POO-main/src/ejercicioClasesRol/Main.java
@@ -0,0 +1,15 @@
+package ejercicioClasesRol;
+
+/**
+ * Clase principal del programa
+ */
+public class Main {
+
+   /**
+    * Método principal del programa
+    * @param args argumentos de entrada
+    */
+   public static void main(String[] args) {
+       System.out.println("Hola mundo");
+   }
+}
